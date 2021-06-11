afde6cb90b0d97cd50fce470a9c960ce880b93ba
madatoo <magda.ruszaj@gmail.com>
2021-06-11 00:57:27 +0000

    removed secret key

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-a-x$pc%mi2)2kyo!r$n*fh737%6u6m_o=pdq^tve*z8xomhl_=')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
